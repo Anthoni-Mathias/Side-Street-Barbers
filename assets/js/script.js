2fc918959b63eae1bee8423848cfb06ee1480012
Anthoni Mathias <63785517+Anthoni-Mathias@users.noreply.github.com>
2024-03-22 17:24:34 +0000

    clean code

--- assets/js/script.js
+++ assets/js/script.js
@@ -35,28 +35,6 @@ const closeNavbar = () => navbar.classList.remove("active");
 addEventOnElem(navLinks, "click", closeNavbar);
 
 
-
-/**
- * header & back top btn active when scroll down to 100px
- */
-
-const header = document.querySelector("[data-header]");
-const backTopBtn = document.querySelector("[data-back-top-btn]");
-
-const headerActive = function () {
-  if (window.scrollY > 100) {
-    header.classList.add("active");
-    backTopBtn.classList.add("active");
-  } else {
-    header.classList.remove("active");
-    backTopBtn.classList.remove("active");
-  }
-}
-
-addEventOnElem(window, "scroll", headerActive);
-
-
-
 /**
  * filter function
  */
